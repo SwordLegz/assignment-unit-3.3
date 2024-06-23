71fa433ac819b53288be10e65eb340b193f07f12
Brii SwordLegz <brii.swordlegz@gmail.com>
2024-06-23 16:28:49 -0500

    realized I was adding quotes for a numbers array when it was not needed, so I adjusted it

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -4,8 +4,8 @@ console.log('****** Part Supply *******');
 // your code does what you expect!
 
 // 1. Create a variable called 'partsNeeded' with a value of the number 40.
-
 console.log('1. Create partsNeeded:');
+
 let partsNeeded = 40;
 console.log('Parts Needed:', partsNeeded);
 
@@ -15,24 +15,31 @@ console.log('Parts Needed:', partsNeeded);
 // 2. Create a variable called 'supplyChanges' whose value is an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11.
 console.log('2. Create supplyChanges:');
-let supplyChanges = ['3', '5', '-6', '0', '7', '11'];
+
+let supplyChanges = [3, 5, -6, 0, 7, 11];
 console.log('Supply Changes:', supplyChanges);
 
-
+//// DONE
 
 // 3. Create a variable called 'secondItem' and assign it the value of the second
 //    item in the 'supplyChanges' array.
 console.log('3. Access the second value of supplyChanges:');
+
 let secondItem = supplyChanges[1];
 console.log('Second item is:', secondItem);
 
+//// DONE
 
 // 4. The last value in the 'supplyChanges' array was added by mistake.
 //    Remove it from the array and store it inside a new variable called 'removedItem'.
 console.log('4. Remove the last value from supplyChanges:');
+
 let removedItem = supplyChanges.pop();
-console.log('Last Item Removed:', removedItem);
-console.log('Supply Changes are now:', supplyChanges)
+supplyChanges.pop();
+console.log('Last Item Removed:', supplyChanges);
+console.log('Removed Last Item and stored in its own variable');
+
+//// DONE
 
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
@@ -52,9 +59,9 @@ console.log('Item Added', supplyChanges);
 //      - If the value is a negative number, push it into the 'negatives' array.
 //      - If the value is a zero, push it into the 'zeroes' array.
 console.log('6. Looping through supplyChanges to populate arrays with positive, negative, and zero values:');
-// let positives = [];
-// let negatives = [];
-// let zeros = [];
+let positives = [];
+let negatives = [];
+let zeros = [];
 
 
 
